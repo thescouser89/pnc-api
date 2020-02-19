25d6ee8158eb80aca892a1a23bf3e5f9c5fa22ec
Matej Lazar <matejonnet@gmail.com>
2020-02-19 12:35:18 +0100

    Make bifrost.MetaData deserializable.

--- src/main/java/org/jboss/pnc/api/bifrost/dto/MetaData.java
+++ src/main/java/org/jboss/pnc/api/bifrost/dto/MetaData.java
@@ -2,6 +2,9 @@ package org.jboss.pnc.api.bifrost.dto;
 
 import lombok.Getter;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
 /**
  * @author <a href="mailto:matejonnet@gmail.com">Matej Lazar</a>
  */
@@ -10,7 +13,16 @@ public class MetaData {
     @Getter
     private String md5Digest;
 
+    public MetaData() {
+    }
+
     public MetaData(String md5Digest) {
         this.md5Digest = md5Digest;
     }
+
+    public static MetaData fromString(String json) {
+        Jsonb jsonb = JsonbBuilder.create();
+        return jsonb.fromJson(json, MetaData.class);
+    }
+
 }

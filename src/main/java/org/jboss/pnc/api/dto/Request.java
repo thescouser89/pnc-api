4ee9539cd918083bdb945edef2c93168626716c5
Honza Brázdil <jbrazdil@redhat.com>
2021-03-15 14:21:48 +0100

    Cleanup and improve Request class

--- src/main/java/org/jboss/pnc/api/dto/Request.java
+++ src/main/java/org/jboss/pnc/api/dto/Request.java
@@ -18,200 +18,75 @@
 
 package org.jboss.pnc.api.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-import lombok.ToString;
-
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
-@ToString
-@JsonDeserialize(builder = Request.Builder.class)
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+import static org.jboss.pnc.api.constants.HttpHeaders.AUTHORIZATION_STRING;
+
+@Getter
+@AllArgsConstructor
+@Jacksonized
+@Builder(builderClassName = "Builder")
 public class Request {
     private final Method method;
-    @Deprecated
-    private final URL url;
     private final URI uri;
-    private final Set<Header> headers;
+    private final List<Header> headers;
     private final Object attachment;
 
-    /**
-     * @deprecated use URI instead of URL
-     */
-    @Deprecated
-    public Request(String method, URL url) {
-        this(method, url, new HashSet<>(), null);
-    }
-
-    /**
-     * @deprecated use URI instead of URL
-     */
-    @Deprecated
-    public Request(String method, URL url, Set<Header> headers) {
-        this(method, url, headers, null);
-    }
-
-    /**
-     * @deprecated use URI instead of URL
-     */
-    @Deprecated
-    public Request(String method, URL url, Set<Header> headers, Object attachment) {
-        this.method = Method.valueOf(method);
-        this.url = url;
-        this.headers = headers;
-        this.attachment = attachment;
-        URI uri = null;
-        try {
-            if (url != null) {
-                uri = url.toURI();
-            }
-        } catch (URISyntaxException e) {
-            // ignore, every URL should be a valid URI
-            uri = null;
-        }
-        this.uri = uri;
-    }
-
-    @Deprecated
-    public Request(String method, URI uri) {
-        this(method, uri, new HashSet<>(), null);
-    }
-
-    @Deprecated
-    public Request(String method, URI uri, Set<Header> headers) {
-        this(method, uri, headers, null);
-    }
-
-    @Deprecated
-    public Request(String method, URI uri, Set<Header> headers, Object attachment) {
-        this.method = Method.valueOf(method);
-        this.url = null;
-        this.uri = uri;
-        this.headers = headers;
-        this.attachment = attachment;
-    }
-
     public Request(Method method, URI uri) {
-        this(method, uri, new HashSet<>(), null);
+        this(method, uri, new ArrayList<>(), null);
     }
 
-    public Request(Method method, URI uri, Set<Header> headers) {
+    public Request(Method method, URI uri, List<Header> headers) {
         this(method, uri, headers, null);
-    }
-
-    public Request(Method method, URI uri, Set<Header> headers, Object attachment) {
-        this.method = method;
-        this.url = null;
-        this.uri = uri;
-        this.headers = headers;
-        this.attachment = attachment;
-    }
-
-    public Method getMethod() {
-        return method;
-    }
-
-    /**
-     * @deprecated use URI instead of URL
-     */
-    @Deprecated
-    public URL getUrl() {
-        return url;
-    }
-
-    public URI getUri() {
-        return uri;
-    }
-
-    public Set<Header> getHeaders() {
-        return headers;
-    }
-
-    public Object getAttachment() {
-        return attachment;
-    }
-
-    @JsonPOJOBuilder(withPrefix = "")
-    public static final class Builder {
-        private String method;
-        @Deprecated
-        private URL url;
-        private URI uri;
-        private Set<Header> headers;
-        private Object attachment;
-
-        private Builder() {
-        }
-
-        public Builder method(String method) {
-            this.method = method;
-            return this;
-        }
-
-        /**
-         * @deprecated use URI instead of URL
-         */
-        @Deprecated
-        public Builder url(URL url) {
-            this.url = url;
-            return this;
-        }
-
-        public Builder uri(URI uri) {
-            this.uri = uri;
-            return this;
-        }
-
-        public Builder headers(Set<Header> headers) {
-            this.headers = headers;
-            return this;
-        }
-
-        public Builder addHeader(Header header) {
-            this.headers.add(header);
-            return this;
-        }
-
-        public Builder attachment(Object attachment) {
-            this.attachment = attachment;
-            return this;
-        }
-
-        public Request build() {
-            if (url != null && uri == null) {
-                return new Request(method, url, headers, attachment);
-            } else {
-                return new Request(method, uri, headers, attachment);
-            }
-        }
     }
 
     public enum Method {
         GET, POST, PUT, DELETE, HEAD;
     }
 
-    @ToString
+    public static class Builder {
+        private List<Header> headers = new ArrayList<>(); // initialize default value for lombok builder
+
+        public Builder header(Header header) {
+            headers.add(header);
+            return this;
+        }
+
+        public Builder header(String name, String value) {
+            return header(new Header(name, value));
+        }
+
+        public Builder authTokenHeader(String token) {
+            return header(AUTHORIZATION_STRING, "Bearer " + token);
+        }
+    }
+
+    @Value
+    @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE) // To make Jackson happy
+    @AllArgsConstructor
     public static class Header {
-        private String name;
-        private String value;
+        private final String name;
+        private final String value;
 
-        Header() {
-        }
-
-        public Header(String name, String value) {
-            this.name = name;
-            this.value = value;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getValue() {
-            return value;
+        @Override
+        public String toString() {
+            if (AUTHORIZATION_STRING.equals(getName())) {
+                return getName() + ":***";
+            } else {
+                return getName() + ":" + getValue();
+            }
         }
     }
 }
--- src/test/java/org/jboss/pnc/api/dto/RequestSerializationTest.java
+++ src/test/java/org/jboss/pnc/api/dto/RequestSerializationTest.java
@@ -20,14 +20,17 @@ package org.jboss.pnc.api.dto;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.jboss.pnc.api.constants.HttpHeaders;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class RequestSerializationTest {
 
@@ -35,12 +38,25 @@ class RequestSerializationTest {
 
     @Test
     public void shouldDeserializeRequest() throws JsonProcessingException, URISyntaxException {
-        Set<Request.Header> headers = new HashSet<>();
+        List<Request.Header> headers = new ArrayList<>();
         headers.add(new Request.Header(HttpHeaders.AUTHORIZATION_STRING, "Bearer 12345"));
         Request request = new Request(Request.Method.GET, new URI("http://localhost/"), headers);
         String serialized = objectMapper.writeValueAsString(request);
 
         Request deserialized = objectMapper.readValue(serialized, Request.class);
         Assertions.assertEquals(Request.Method.GET, deserialized.getMethod());
+    }
+
+    @Test
+    public void testSecurityCensoring() throws URISyntaxException {
+        String token = "top-secret-token";
+        Request request = Request.builder()
+                .uri(new URI("http://foo.bar/"))
+                .method(Request.Method.POST)
+                .header("foo", "bar")
+                .header("Authorization", token)
+                .build();
+        String toString = request.toString();
+        assertFalse(toString.contains(token), "Secret token should be censored");
     }
 }

6fd14bb75dd77bea4b235a6ce2d469994fe7d75d
Dustin Kut Moy Cheung <dcheung@redhat.com>
2022-11-04 16:38:52 -0400

    [NCL-7290] Add DTO for Repour endpoints

---
+++ src/main/java/org/jboss/pnc/api/repour/dto/RepourCloneCallback.java
@@ -0,0 +1,44 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2020 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.api.repour.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Builder(builderClassName = "Builder")
+@AllArgsConstructor
+@ToString(callSuper = true)
+@JsonDeserialize(builder = RepourCloneCallback.Builder.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RepourCloneCallback {
+
+    /**
+     * URL to use to callback
+     */
+    private String url;
+
+    /**
+     * The HTTP method to use on the callback
+     */
+    private String method;
+}

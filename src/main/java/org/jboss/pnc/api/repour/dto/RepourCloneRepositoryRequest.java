b920976b43906677b365b95d7ee8f7491d38659b
Dustin Kut Moy Cheung <dcheung@redhat.com>
2022-11-04 16:38:52 -0400

    [NCL-7290] Add DTO for Repour endpoints

---
+++ src/main/java/org/jboss/pnc/api/repour/dto/RepourCloneRepositoryRequest.java
@@ -0,0 +1,57 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2020 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.api.repour.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * Repour's clone repository DTO
+ */
+@Data
+@Builder(builderClassName = "Builder")
+@AllArgsConstructor
+@ToString(callSuper = true)
+@JsonDeserialize(builder = RepourCloneRepositoryRequest.Builder.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RepourCloneRepositoryRequest {
+
+    /**
+     * Type of scm: usually: git
+     */
+    private String type;
+
+    /**
+     * Original repository to sync from
+     */
+    private String originRepoUrl;
+
+    /**
+     * Repository to sync to
+     */
+    private String targetRepoUrl;
+
+    /**
+     * Callback specification
+     */
+    private RepourCloneCallback callback;
+}
---
+++ src/main/java/org/jboss/pnc/api/repour/dto/RepourCreateRepositoryRequest.java
@@ -0,0 +1,54 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014-2020 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.api.repour.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * DTO for Repour's internal repository creation
+ */
+@Data
+@Builder(builderClassName = "Builder")
+@AllArgsConstructor
+@ToString(callSuper = true)
+@JsonDeserialize(builder = RepourCreateRepositoryRequest.Builder.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RepourCreateRepositoryRequest {
+
+    /**
+     * Repository name
+     */
+    private String project;
+
+    @JsonProperty("owner_groups")
+    private List<String> ownerGroups;
+
+    /**
+     * Set the parent project. Used to set the default access permissions for the repository
+     */
+    @JsonProperty("parent_project")
+    private String parentProject;
+}

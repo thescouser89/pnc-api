dbb01169fbb5ddc63d2afc50a6ff1dc14da6247b
Adam Krídl <akridl@redhat.com>
2023-11-02 11:11:39 +0100

    [NCL-8068]: Update AnalysisResult DTO to use new DelAn API
    The AnalysisResult DTO is sent to orchestrator (from BPM) when the
    Deliverable Analysis finishes in order to inform orchestrator to
    actually store analyzed artifacts in the DB.

--- src/main/java/org/jboss/pnc/api/deliverablesanalyzer/dto/AnalysisResult.java
+++ src/main/java/org/jboss/pnc/api/deliverablesanalyzer/dto/AnalysisResult.java
@@ -1,21 +1,24 @@
 package org.jboss.pnc.api.deliverablesanalyzer.dto;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.extern.jackson.Jacksonized;
+
+import java.util.List;
 
 @Data
 @Jacksonized
 @Builder(builderClassName = "Builder")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AnalysisResult {
+
     @NonNull
-    private final String milestoneId;
+    private final String operationId;
+
     @NonNull
     private final List<FinderResult> results;
+
+    private final boolean scratch;
 }
